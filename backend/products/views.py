7db5ccdc2283717b2545479fda41c62670f9cdf5
rewen9 <n.revin@ekt.ancor.ru>
2023-12-06 21:41:21 +0500

    ProductsViewSet поправлен

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.request import Request
 
 from backend.products.models import (
     Products,
@@ -15,7 +16,7 @@ import logging
 class ProductsViewSet(viewsets.ModelViewSet):
     
     @action(methods=['get'], detail=False)
-    def get_products(self, request):
+    def get_products(self, request: Request):
         js = None
         res_status = status.HTTP_200_OK
         
@@ -27,7 +28,7 @@ class ProductsViewSet(viewsets.ModelViewSet):
         return Response(js, status=res_status)
 
     @action(methods=['get'], detail=False, url_path='products_max_price')
-    def get_products_under_max_price(self, request):
+    def get_products_under_max_price(self, request: Request):
         """Получить список продуктов не более указанной суммы"""
         res_status = status.HTTP_200_OK
         
@@ -37,8 +38,9 @@ class ProductsViewSet(viewsets.ModelViewSet):
             products = Products.objects.select_related('products_price', 'products_count').filter(
                 id__in=products
             )
-            results = []
+            
             # Итерируемся по отфильтрованным товарам и получаем нужные данные
+            results = []
             for product in products:
                 product_data = {
                     'id': product.pk,
@@ -48,30 +50,32 @@ class ProductsViewSet(viewsets.ModelViewSet):
                 }
                 results.append(product_data)
         except Exception as err:
+            logging.info('ошибка в получении продуктов не более указанной суммы', err)
             res_status = status.HTTP_400_BAD_REQUEST
+            
         return Response(results, status=res_status)
 
 
     @action(methods=['POST'], detail=False)
-    def product(self, request):
-        for js_data in request.data:
-            name = js_data.get('name') 
-            count = js_data.get('count') or 0
-            price = js_data.get('price') or 0
-            try:
-                new_product = Products.objects.create(name=name)
-                Products_price.objects.create(
-                    product=new_product,
-                    price=price
-                )
-                Products_count.objects.create(
-                    product=new_product,
-                    available_quantity=count
-                )
-                res_status = status.HTTP_200_OK
-            except Exception as er:
-                logging.info('ошибка в создании продукта', er)
-                res_status = status.HTTP_400_BAD_REQUEST
+    def product(self, request: Request):
+        name = request.query_params.get('name') 
+        count = request.query_params.get('count') or 0
+        price = request.query_params.get('price') or 0
+        try:
+            new_product = Products.objects.create(name=name)
+            Products_price.objects.create(
+                product=new_product,
+                price=price
+            )
+            Products_count.objects.create(
+                product=new_product,
+                available_quantity=count
+            )
+            res_status = status.HTTP_200_OK
+        except Exception as er:
+            logging.info('ошибка в создании продукта', er)
+            res_status = status.HTTP_400_BAD_REQUEST
+            
         return Response(status=res_status)
     
     
