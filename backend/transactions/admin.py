8874300b84fc899979f66633e992a4bafa439b01
byrew <byrew@byrew.ru>
2023-12-19 00:11:37 +0500

    добавлена модель транзакций

--- backend/transactions/admin.py
+++ backend/transactions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from backend.transactions.models import Transactions
+from backend.admin import ADMIN_SITE
 
-# Register your models here.
+@admin.register(Transactions, site=ADMIN_SITE)
+class TransactioionsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'amount', 'currency', 'status', 'created_at']
+    list_filter = ['customer']
